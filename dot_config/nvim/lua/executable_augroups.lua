66303607a7398f261454d1ce264c3b81a16779cd
Phil <philip.shaughnessy@gmail.com>
2022-04-17 10:03:16 -0400

    update settings, formatting

--- dot_config/nvim/lua/executable_augroups.lua
+++ dot_config/nvim/lua/executable_augroups.lua
@@ -20,31 +20,31 @@ local autocmds = {
 			{ "VimEnter",					"*",		"cwindow" };
 		};
 
-    save_shada = {
-        {"VimLeave", "*", "wshada!"};
-    };
-    resize_windows_proportionally = {
-        { "VimResized", "*", ":wincmd =" };
-    };
-    toggle_search_highlighting = {
-        { "InsertEnter", "*", "setlocal nohlsearch" };
-    };
-    -- ansi_esc_log = {
-    --     { "BufEnter", "*.log", ":AnsiEsc" };
-    -- };
+	save_shada = {
+		{"VimLeave", "*", "wshada!"};
+	};
+	resize_windows_proportionally = {
+		{ "VimResized", "*", ":wincmd =" };
+	};
+	toggle_search_highlighting = {
+		{ "InsertEnter", "*", "setlocal nohlsearch" };
+	};
+	-- ansi_esc_log = {
+	--	 { "BufEnter", "*.log", ":AnsiEsc" };
+	-- };
 }
 
 --- This function is taken from https://github.com/norcalli/nvim_utils
 local function nvim_create_augroups(definitions)
-    for group_name, definition in pairs(definitions) do
-        api.nvim_command('augroup '..group_name)
-        api.nvim_command('autocmd!')
-        for _, def in ipairs(definition) do
-            local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
-            api.nvim_command(command)
-        end
-        api.nvim_command('augroup END')
-    end
+	for group_name, definition in pairs(definitions) do
+		api.nvim_command('augroup '..group_name)
+		api.nvim_command('autocmd!')
+		for _, def in ipairs(definition) do
+			local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
+			api.nvim_command(command)
+		end
+		api.nvim_command('augroup END')
+	end
 end
 
 nvim_create_augroups(autocmds)
