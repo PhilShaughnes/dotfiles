e6facf550d5750103776ca3ae85092625381a0bb
Phil <philip.shaughnessy@gmail.com>
2023-01-13 22:26:57 -0500

    Add lazy config

---
+++ dot_config/nvim/lua/plugin.lua
@@ -0,0 +1,43 @@
+-- bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+-- initalize lazy and load plugins
+require("lazy").setup("plugs", {
+	defaults = {
+		lazy = false, -- should plugins be lazy-loaded?
+	},
+	change_detection = {
+		enabled = false,
+		notify = false,
+	},
+	install = {
+		-- try to load one of these colorschemes when starting an installation during startup
+		colorscheme = { "doom-one", "slate" },
+	},
+	performance = {
+		rtp = {
+			---@type string[] list any plugins you want to disable here
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				-- "tutor",
+				"zipPlugin",
+			},
+		},
+	},
+})
