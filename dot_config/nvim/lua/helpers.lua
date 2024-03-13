ae5b91836ac518ff7b377c62482b89b778a1f16c
Phil Shaughnessy <philip.shaughnessy@gmail.com>
2024-03-13 09:34:54 -0400

    Adds stuff

--- dot_config/nvim/lua/helpers.lua
+++ dot_config/nvim/lua/helpers.lua
@@ -65,4 +65,64 @@ end
 _G.dump = M.dump
 _G.trim_ws = M.trim_ws
 
+local lineLength = function(lnum)
+	local tab_hex_code = 9
+
+	-- TODO: tabsize might be different than shiftwidth
+	local expandtab    = vim.api.nvim_buf_get_option(0, "expandtab")
+	local tabsize      = vim.api.nvim_buf_get_option(0, "shiftwidth")
+	local line         = vim.fn.getline(lnum)
+	local length       = #line
+
+	if expandtab then
+		return length
+	end
+
+	-- count tabs used as indentation
+	for i = 1, #line do
+		if line:byte(i) ~= tab_hex_code then
+			break
+		end
+		length = length + tabsize - 1
+	end
+
+	return length
+end
+
+local move = function(key)
+	-- NOTE: use norm command because nvim_win_set_cursor doesnt support virtual edit
+
+	local row, col = vim.fn.line('.'), vim.fn.virtcol('.')
+
+	local first_lnum = vim.fn.line('0')
+	local last_lnum = vim.fn.line('$')
+
+	local diff = key == 'k' and -1 or 1
+	local lnum = row + diff
+	local cnt = -1
+
+	while lnum >= first_lnum and lnum <= last_lnum + 1 do
+		cnt = cnt + 1
+		if lineLength(lnum) < col then break end
+		lnum = lnum + diff
+	end
+
+	if cnt ~= 0 then
+		vim.cmd("norm " .. cnt .. key)
+		return
+	end
+
+	while lnum >= first_lnum and lnum <= last_lnum + 1 do
+		cnt = cnt + 1
+		if lineLength(lnum) >= col then break end
+		lnum = lnum + diff
+	end
+
+	vim.cmd("norm " .. cnt .. key)
+end
+
+function M.go_up() move('k') end
+
+function M.go_down() move('j') end
+
 return M
--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,88 @@
 local g = vim.g
 
+local ensure_install_langs = { "ruby", "bash", "c", "cmake", "comment", "css",
+	"dockerfile", "eex", "elixir", "erlang", "glimmer", "go", "graphql",
+	"heex", "html", "hurl", "java", "javascript", "jsdoc", "json", "lua",
+	"make", "python", "regex", "scss", "markdown", "vimdoc",
+	"typescript", "tsx", "vim", "vue", "toml", "yaml", "hcl",
+	"markdown_inline", "cue" }
+
+local ts_textobjects = {
+	select = {
+		enable = true,
+
+		-- Automatically jump forward to textobj, similar to targets.vim
+		lookahead = true,
+		keymaps = {
+			-- You can use the capture groups defined in textobjects.scm
+			["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+			["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+			["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+			["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+
+			["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
+			["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
+
+			["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+			["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+
+			["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+			["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+
+			["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
+			["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
+
+			["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
+			["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
+
+			["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+			["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+		},
+	},
+}
+
+local incremental = {
+	enable = true,
+	keymaps = {
+		init_selection = "<C-t>",
+		node_incremental = "<C-t>",
+		scope_incremental = false,
+		node_decremental = "<bs>",
+	},
+}
+
+local playground = {
+	enable = true,
+	disable = {},
+	updatetime = 25,        -- Debounced time for highlighting nodes in the playground from source code
+	persist_queries = false, -- Whether the query persists across vim sessions
+	keybindings = {
+		toggle_query_editor = 'o',
+		toggle_hl_groups = 'i',
+		toggle_injected_languages = 't',
+		toggle_anonymous_nodes = 'a',
+		toggle_language_display = 'I',
+		focus_language = 'f',
+		unfocus_language = 'F',
+		update = 'R',
+		goto_node = '<cr>',
+		show_help = '?',
+	},
+}
+
+
 local M = {
 	-- { 'RRethy/nvim-treesitter-endwise' },
+	{
+		'nvim-treesitter/nvim-treesitter-textobjects',
+		lazy = true,
+	},
 	{
 		'nvim-treesitter/nvim-treesitter',
 		lazy = true,
 		dependencies = {
 			-- 'RRethy/nvim-treesitter-endwise',
+			'nvim-treesitter/nvim-treesitter-textobjects'
 		},
 		config = function()
 			vim.treesitter.language.register('make', 'just')
@@ -17,12 +93,7 @@ local M = {
 			}
 			require 'nvim-treesitter.configs'.setup {
 				-- one of "all", "maintained" (parsers with maintainers), or a list of languages
-				ensure_installed = { "ruby", "bash", "c", "cmake", "comment", "css",
-					"dockerfile", "eex", "elixir", "erlang", "glimmer", "go", "graphql",
-					"heex", "html", "hurl", "java", "javascript", "jsdoc", "json", "lua",
-					"make", "python", "regex", "scss", "markdown", "vimdoc",
-					"typescript", "tsx", "vim", "vue", "toml", "yaml", "hcl",
-					"markdown_inline", "cue" },
+				ensure_installed = ensure_install_langs,
 				ignore_install = { "rust" },
 				highlight = {
 					enable = true,   -- false will disable the whole extension
@@ -30,34 +101,10 @@ local M = {
 					disable = { "rust" }, -- list of language that will be disabled
 				},
 				-- endwise = { enable = true },
-				-- incremental_selection = {
-				--   enable = true,
-				--   keymaps = {
-				--     init_selection = "gnn",
-				--     node_incremental = "grn",
-				--     scope_incremental = "grc",
-				--     node_decremental = "grm",
-				--   },
-				-- },
+				incremental_selection = incremental,
+				textobjects = ts_textobjects,
 				indent = { enable = true },
-				playground = {
-					enable = true,
-					disable = {},
-					updatetime = 25,    -- Debounced time for highlighting nodes in the playground from source code
-					persist_queries = false, -- Whether the query persists across vim sessions
-					keybindings = {
-						toggle_query_editor = 'o',
-						toggle_hl_groups = 'i',
-						toggle_injected_languages = 't',
-						toggle_anonymous_nodes = 'a',
-						toggle_language_display = 'I',
-						focus_language = 'f',
-						unfocus_language = 'F',
-						update = 'R',
-						goto_node = '<cr>',
-						show_help = '?',
-					},
-				}
+				playground = playground,
 			}
 
 			-- cmd 'set foldmethod=expr'
